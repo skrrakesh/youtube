e841d070832d7eb8264b2dcd99c056a2a96baf7c
skrrakesh <skrrakesh7@gmail.com>
2023-05-29 17:46:11 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1 style={{backgroundColor:"red",color:'white'}}>YouTube</h1>
+      {/* <h1 style={{backgroundColor:"red",color:'white'}}>YouTube</h1> */}
+      <h1>YouTube</h1>
+      <Navbar />
     </div>
   );
 }
